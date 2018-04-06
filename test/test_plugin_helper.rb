b0151c8d5168b271456b6f54c75a22708e985c53
Timo Goebel <mail@timogoebel.name>
2018-04-06 21:17:39 +0200

    add travis-ci

--- test/test_plugin_helper.rb
+++ test/test_plugin_helper.rb
@@ -2,5 +2,5 @@
 require 'test_helper'
 
 # Add plugin to FactoryBot's paths
-FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
-FactoryBot.reload
+FactoryGirl.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
+FactoryGirl.reload
