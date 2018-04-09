96e9109d3322f147bc77a1072449f1f937bca8be
Timo Goebel <mail@timogoebel.name>
2018-04-09 16:13:04 +0200

    add basic controller tests

---
+++ test/factories/host.rb
@@ -0,0 +1,38 @@
+FactoryGirl.modify do
+  factory :host do
+    trait :with_vmware_facet do
+      vmware_facet
+    end
+
+    trait :with_vmware_hypervisor_facet do
+      vmware_hypervisor_facet
+    end
+
+    trait :with_vmware_tools_status do
+      with_vmware_facet
+      after(:create) do |host, _evaluator|
+        create :vmware_tools_status, host: host
+      end
+    end
+
+    trait :with_vmware_operatingsystem_status do
+      with_vmware_facet
+      after(:create) do |host, _evaluator|
+        create :vmware_operatingsystem_status, host: host
+      end
+    end
+
+    trait :with_vmware_cpu_hot_add_status do
+      with_vmware_facet
+      after(:create) do |host, _evaluator|
+        create :vmware_cpu_hot_add_status, host: host
+      end
+    end
+
+    trait :with_wreckingball_statuses do
+      with_vmware_tools_status
+      with_vmware_operatingsystem_status
+      with_vmware_cpu_hot_add_status
+    end
+  end
+end
