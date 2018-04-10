3b255b4c141f29d4ee7ba52e7a32ded0b802c591
Timo Goebel <mail@timogoebel.name>
2018-04-10 10:20:00 +0200

    add basic unit tests

---
+++ test/unit/foreman_wreckingball/vmware_cluster_importer_test.rb
@@ -0,0 +1,42 @@
+require 'test_plugin_helper'
+
+module ForemanWreckingball
+  class VmwareClusterImporterTest < ActiveSupport::TestCase
+    setup do
+      User.current = users(:admin)
+      Fog.mock!
+    end
+    teardown { Fog.unmock! }
+
+    let(:compute_resource) { FactoryGirl.create(:vmware_cr, :uuid => 'Solutions') }
+    let(:importer) do
+      VmwareClusterImporter.new(
+        compute_resource: compute_resource
+      )
+    end
+
+    describe '#import!' do
+      test 'imports clusters' do
+        importer.import!
+        clusters = ForemanWreckingball::VmwareCluster.pluck(:name)
+        assert_includes clusters, 'Solutionscluster'
+        assert_includes clusters, 'Problemscluster'
+        assert_includes clusters, 'Nested/Lastcluster'
+      end
+
+      test 'removes old clusters' do
+        old_cluster = FactoryGirl.create(:vmware_cluster, compute_resource: compute_resource)
+        importer.import!
+        refute ForemanWreckingball::VmwareCluster.find_by(id: old_cluster.id)
+      end
+
+      test 'can be run twice without a change' do
+        importer.import!
+        before = ForemanWreckingball::VmwareCluster.pluck(:updated_at)
+        importer.import!
+        after = ForemanWreckingball::VmwareCluster.pluck(:updated_at)
+        assert_equal before, after
+      end
+    end
+  end
+end
---
+++ test/unit/foreman_wreckingball/vmware_hypervisor_importer_test.rb
@@ -0,0 +1,72 @@
+require 'test_plugin_helper'
+
+module ForemanWreckingball
+  class VmwareHypervisorImporterTest < ActiveSupport::TestCase
+    setup do
+      User.current = users(:admin)
+      Fog.mock!
+    end
+    teardown { Fog.unmock! }
+
+    let(:organization) do
+      Organization.find_by(name: 'Organization 1')
+    end
+    let(:location) do
+      Location.find_by(name: 'Location 1')
+    end
+    let(:compute_resource) do
+      FactoryGirl.create(
+        :vmware_cr,
+        uuid: 'Solutions',
+        organizations: [organization],
+        locations: [location]
+      )
+    end
+    let(:cluster) do
+      FactoryGirl.create(
+        :vmware_cluster,
+        name: 'Solutionscluster',
+        compute_resource: compute_resource
+      )
+    end
+    let(:importer) do
+      VmwareHypervisorImporter.new(
+        compute_resource: compute_resource
+      )
+    end
+
+    describe '#import!' do
+      setup do
+        cluster
+      end
+
+      test 'imports hypervisors' do
+        importer.import!
+        host = Host::Managed.joins(:vmware_hypervisor_facet).find_by(:name => 'host1.example.com')
+
+        # Test host attributes are set correctly
+        assert_equal 'example.com', host.domain.name
+        assert_equal 'PowerEdge R730', host.model.name
+        assert_equal '1.2.3.4', host.ip
+        assert_nil host.ip6
+        assert_equal organization, host.organization
+        assert_equal location, host.location
+
+        # Test facet attributes are set correctly
+        assert_equal cluster, host.vmware_hypervisor_facet.vmware_cluster
+        assert_equal 20, host.vmware_hypervisor_facet.cpu_cores
+        assert_equal 40, host.vmware_hypervisor_facet.cpu_threads
+        assert_equal 2, host.vmware_hypervisor_facet.cpu_sockets
+        assert_equal 824_597_241_856, host.vmware_hypervisor_facet.memory
+        assert_equal '4c4c4544-0051-3610-8046-c4c44f584a32', host.vmware_hypervisor_facet.uuid
+      end
+
+      test 'updates host by katello name' do
+        host = FactoryGirl.create(:host,  organization: organization)
+        host.update_attributes!(:name => "virt-who-host1.example.com-#{organization.id}")
+        importer.import!
+        assert_equal 'host1.example.com', host.reload.name
+      end
+    end
+  end
+end
