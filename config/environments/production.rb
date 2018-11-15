09a7da6dd2c9218f19543684c4dd1faebc8ffc81
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2018-11-15 15:34:59 +0100

    Fix compiling assets in production

---
+++ config/environments/production.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require Rails.root.join('config', 'environments', 'production.rb')
+
+Foreman::Application.configure do
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+end
