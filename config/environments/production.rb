7d9e138c0d310d9815ad7dd5bcec2a7f9ca2c568
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2018-11-21 17:11:11 +0100

    Fix Uglifier issue on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,5 +3,5 @@
 require Rails.root.join('config', 'environments', 'production.rb')
 
 Foreman::Application.configure do
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  config.assets.js_compressor = Uglifier.new(harmony: true) if defined?(Uglifier)
 end
