a659b55535805811e60c71143554597b897ad1b7
Sebastian Ziebell <sebastian.ziebell@dm.de>
2018-11-21 13:43:26 +0100

    Merge query parts into a single one

--- app/controllers/foreman_wreckingball/hosts_controller.rb
+++ app/controllers/foreman_wreckingball/hosts_controller.rb
@@ -54,13 +54,12 @@ module ForemanWreckingball
     def status_hosts
       @status = STATUSES_MAP[params.fetch(:status).to_sym]
 
-      query = Host.authorized(:view_hosts, Host)
-                  .joins(@status.host_association)
-                  .includes(@status.host_association, :vmware_facet, :environment)
-
-      all_hosts = query.where.not('host_status.status': @status.global_ok_list)
-                       .preload(:owner)
-                       .order(:name)
+      all_hosts = Host.authorized(:view_hosts, Host)
+                      .joins(@status.host_association)
+                      .includes(@status.host_association, :vmware_facet, :environment)
+                      .where.not('host_status.status': @status.global_ok_list)
+                      .preload(:owner)
+                      .order(:name)
 
       @count = all_hosts.size
       @hosts = all_hosts.paginate(page: params.fetch(:page, 1), per_page: params.fetch(:per_page, 100))
