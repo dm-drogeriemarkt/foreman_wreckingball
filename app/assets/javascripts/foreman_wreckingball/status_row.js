123a4e8f2994d9b7f7e6dcba6418849d5edf2eb2
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2018-12-04 11:57:43 +0100

    Fix expanding list of status hosts (#69)
    * Fix expanding list of status hosts
    * Fix minor bug with actions menu

---
+++ app/assets/javascripts/foreman_wreckingball/status_row.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+  // click the list-view heading then expand a row
+  $('.list-group-item-header').click(function(event) {
+    if(!$(event.target).is('button, a, input, .fa-ellipsis-v')) {
+      $(this).find('.fa-angle-right').toggleClass('fa-angle-down')
+        .end().parent().toggleClass('list-view-pf-expand-active')
+        .find('.list-group-item-container').toggleClass('hidden');
+    }
+  });
+
+  // click the close button, hide the expand row and remove the active status
+  $('.list-group-item-container .close').click(function() {
+    $(this).parent().addClass('hidden')
+      .parent().removeClass('list-view-pf-expand-active')
+      .find('.fa-angle-right').removeClass('fa-angle-down');
+  });
+});
