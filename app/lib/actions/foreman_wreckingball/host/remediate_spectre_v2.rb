acc1ed700c5902e212c3766bd79ad37018dafe14
Timo Goebel <mail@timogoebel.name>
2018-12-21 13:40:46 +0100

    spectre v2: provide a remediate action

---
+++ app/lib/actions/foreman_wreckingball/host/remediate_spectre_v2.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module Actions
+  module ForemanWreckingball
+    module Host
+      class RemediateSpectreV2 < Actions::EntryAction
+        middleware.use Actions::Middleware::KeepCurrentUser
+
+        def delay(delay_options, host)
+          action_subject(host)
+          super(delay_options, host)
+        end
+
+        def plan(host)
+          action_subject(host)
+          plan_self
+        end
+
+        def run
+          host = ::Host.find(input[:host][:id])
+
+          initially_powered_on = host.power.ready?
+          output[:initially_powered_on] = initially_powered_on
+
+          vm = host.compute_object
+
+          if initially_powered_on
+            vm.stop
+            vm.wait_for { power_state == 'poweredOff' }
+            fail _('Could not shut down VM.') if vm.ready?
+          end
+
+          vm.start if vm && initially_powered_on
+
+          state = host.refresh_vmware_facet!
+          output[:state] = state
+        end
+
+        def humanized_name
+          _('Power-Cycle VM')
+        end
+
+        def humanized_input
+          input[:host] && input[:host][:name]
+        end
+
+        def append_error(message)
+          output[:errors] ||= []
+          output[:errors] << message
+        end
+
+        def rescue_strategy
+          Dynflow::Action::Rescue::Skip
+        end
+      end
+    end
+  end
+end
---
+++ test/actions/foreman_wreckingball/host/remediate_spectre_v2_test.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'test_plugin_helper'
+
+module Actions
+  module ForemanWreckingball
+    module Vmware
+      class RemediateSpectreV2Test < ActiveSupport::TestCase
+        include ::Dynflow::Testing
+        setup do
+          ::Fog.mock!
+          # this is not stubbed correctly in fog-vsphere
+          Fog::Compute::Vsphere::Server.any_instance.stubs(:cpuHotAddEnabled).returns(false)
+          Fog::Compute::Vsphere::Server.any_instance.stubs(:hardware_version).returns('vmx-13')
+          ::ForemanWreckingball::SpectreV2Status.any_instance.stubs(:recent_hw_version?).returns(true)
+          ::PowerManager::Virt.any_instance.stubs(:ready?).returns(true)
+          Setting::Wreckingball.load_defaults
+        end
+        teardown { ::Fog.unmock! }
+
+        let(:compute_resource) do
+          cr = FactoryBot.create(:compute_resource, :vmware, :with_taxonomy, :uuid => 'Solutions')
+          ComputeResource.find(cr.id)
+        end
+        let(:uuid) { '5032c8a5-9c5e-ba7a-3804-832a03e16381' }
+        let(:vm) { compute_resource.find_vm_by_uuid(uuid) }
+
+        let(:host) do
+          FactoryBot.create(
+            :host,
+            :managed,
+            :with_vmware_facet,
+            compute_resource: compute_resource,
+            uuid: uuid
+          )
+        end
+
+        let(:action_class) { ::Actions::ForemanWreckingball::Host::RemediateSpectreV2 }
+        let(:action) do
+          create_action(action_class).tap do |action|
+            action.stubs(:action_subject).returns(host)
+            action.input.update(
+              host: {
+                id: host.id
+              }
+            )
+          end
+        end
+        let(:planned_action) do
+          plan_action(action, host)
+        end
+        let(:runned_action) { run_action(planned_action) }
+
+        test "it remediates the host's spectre v2 status" do
+          assert_equal :success, runned_action.state
+          assert_equal true, runned_action.output.fetch('state')
+          assert_equal true, runned_action.output.fetch('initially_powered_on')
+        end
+      end
+    end
+  end
+end
