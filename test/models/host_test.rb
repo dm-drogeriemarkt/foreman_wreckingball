80531e5c8d8d4b70417c8ad8476a44068d768cf3
Timo Goebel <mail@timogoebel.name>
2019-01-20 11:57:45 +0100

    add scoped search definitions to vmware facet

--- test/models/host_test.rb
+++ test/models/host_test.rb
@@ -29,4 +29,45 @@ class Host::ManagedTest < ActiveSupport::TestCase
       assert_equal expected, actual
     end
   end
+
+  context 'scoped search' do
+    setup do
+      @host = FactoryBot.create(:host, :with_vmware_facet)
+    end
+
+    test 'search by hardware_version' do
+      hosts = Host.search_for('vsphere_hardware_version = vmx-10')
+      assert_includes hosts, @host
+    end
+
+    test 'search by power_state' do
+      hosts = Host.search_for('vsphere_power_state = poweredOn')
+      assert_includes hosts, @host
+    end
+
+    test 'search by tools_state' do
+      hosts = Host.search_for('vsphere_tools_state = toolsOk')
+      assert_includes hosts, @host
+    end
+
+    test 'search by guest_id' do
+      hosts = Host.search_for('vsphere_guest_id = rhel6_64Guest')
+      assert_includes hosts, @host
+    end
+
+    test 'search by cpus' do
+      hosts = Host.search_for('vsphere_cpus = 2')
+      assert_includes hosts, @host
+    end
+
+    test 'search by corespersocket' do
+      hosts = Host.search_for('vsphere_corespersocket = 1')
+      assert_includes hosts, @host
+    end
+
+    test 'search by memory_mb' do
+      hosts = Host.search_for('vsphere_memory_mb = 8192')
+      assert_includes hosts, @host
+    end
+  end
 end
