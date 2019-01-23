a2385e0178580e238c2e3b75682df50c2da1b05c
Timo Goebel <mail@timogoebel.name>
2019-01-23 20:34:06 +0100

    fix integration test

--- test/integration/hosts_status_managed_hosts_test.rb
+++ test/integration/hosts_status_managed_hosts_test.rb
@@ -21,8 +21,8 @@ class HostsStatusManagedHostsTest < ActionDispatch::IntegrationTest
   end
 
   test 'shows missing host without VMware vm' do
-    managed_host = FactoryBot.create(:host, :managed, :with_vmware_facet, compute_resource: cr, owner: admin, uuid: 1)
-    missing_host = FactoryBot.create(:host, :managed, :with_vmware_facet, compute_resource: cr, owner: admin, uuid: 2)
+    managed_host = FactoryBot.create(:host, :managed, compute_resource: cr, owner: admin, uuid: 1)
+    missing_host = FactoryBot.create(:host, :managed, compute_resource: cr, owner: admin, uuid: 2)
 
     mock_vm = mock('vm')
     mock_vm.stubs(:uuid).returns(managed_host.uuid)
@@ -62,8 +62,11 @@ class HostsStatusManagedHostsTest < ActionDispatch::IntegrationTest
       locations: [tax_location]
     )
 
-    managed1_host = FactoryBot.create(:host, :managed, compute_resource: cr, owner: admin, uuid: 2)
-    managed2_host = FactoryBot.create(:host, :managed, compute_resource: other_cr, owner: admin, uuid: 1)
+    # Host 2 is associated to other_cr, but shows up on cr
+    # Host 1 and 3 are correctly associated.
+    managed1_host = FactoryBot.create(:host, :managed, compute_resource: cr, owner: admin, uuid: 1)
+    managed2_host = FactoryBot.create(:host, :managed, compute_resource: other_cr, owner: admin, uuid: 2)
+    managed3_host = FactoryBot.create(:host, :managed, compute_resource: other_cr, owner: admin, uuid: 3)
 
     mock1_vm = mock('vm1')
     mock1_vm.stubs(:uuid).returns(managed1_host.uuid)
@@ -71,12 +74,20 @@ class HostsStatusManagedHostsTest < ActionDispatch::IntegrationTest
     mock2_vm = mock('vm2')
     mock2_vm.stubs(:uuid).returns(managed2_host.uuid)
     mock2_vm.stubs(:name).returns(managed2_host.name)
-    Foreman::Model::Vmware.any_instance.stubs(:vms).returns([mock1_vm, mock2_vm])
+    mock3_vm = mock('vm3')
+    mock3_vm.stubs(:uuid).returns(managed3_host.uuid)
+    mock3_vm.stubs(:name).returns(managed3_host.name)
+
+    cr.stubs(:vms).returns([mock1_vm, mock2_vm])
+    other_cr.stubs(:vms).returns([mock3_vm])
+
+    ComputeResource.stubs(:where).returns([cr, other_cr])
 
     visit status_managed_hosts_dashboard_hosts_path
 
     list = page.find('#different_vms')
-    assert_includes list.text, managed1_host.name
-    refute_includes list.text, managed2_host.name
+    refute_includes list.text, managed1_host.name
+    assert_includes list.text, managed2_host.name
+    refute_includes list.text, managed3_host.name
   end
 end
