728c364b2cc2d178a9147dc0397ba0a9d8942a0a
Manuel Laug <manuel.laug@dm.de>
2021-03-17 08:58:39 +0100

    Drop Foreman 1.23 and older / add Foreman 2.4 to tests

--- lib/foreman_wreckingball/engine.rb
+++ lib/foreman_wreckingball/engine.rb
@@ -42,7 +42,7 @@ module ForemanWreckingball
 
     initializer 'foreman_wreckingball.register_plugin', :before => :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_wreckingball do
-        requires_foreman '>= 1.21'
+        requires_foreman '>= 1.24'
 
         automatic_assets(false)
         precompile_assets(
